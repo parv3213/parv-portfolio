54bdf6cc162452dcc3f6d906fa7592c94a7c9c59
Parv3213 <parv3213@gmail.com>
2023-01-08 00:19:58 +0530

    Complete Project

---
+++ components/Projects.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+import Project from './Project'
+
+const Projects = () => {
+  return (
+    <motion.div
+      initial={{
+        y: -100,
+        opacity: 0,
+      }}
+      whileInView={{ y: 0, opacity: 1 }}
+      transition={{ duration: 1.2 }}
+      viewport={{ once: true }}
+      className="relative mx-auto flex h-screen min-h-screen max-w-[2000px] flex-col items-center justify-center text-center md:text-left xl:flex-row
+xl:space-y-0 xl:px-10">
+      <h3 className="absolute top-24 text-lg uppercase tracking-[15px] text-zinc-500 sm:text-2xl sm:tracking-[20px]">
+        Projects
+      </h3>
+
+      <div className="absolute top-[30%] left-0 h-[40vh] w-full -skew-y-12 bg-yellow-500/10" />
+
+      <div className="relative z-20 flex w-full snap-x snap-mandatory overflow-y-hidden overflow-x-scroll">
+        <Project />
+        <Project />
+        <Project />
+        <Project />
+      </div>
+    </motion.div>
+  )
+}
+
+export default Projects
