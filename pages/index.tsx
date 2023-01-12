5aa0d1bd223972786cfba933fa5b967fa85b67f4
Parv3213 <parv3213@gmail.com>
2023-01-13 04:10:25 +0530

    Update favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,6 +31,9 @@ const Home = ({ pageInfo, experiences, socials, projects, skills }: Props) => {
           content="Parv is a web3 full-stack developer. Skilled in software engineering and programming."
         />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
       </Head>
 
       <Header socials={socials} />
