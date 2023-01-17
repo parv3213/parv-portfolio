5bc51acbdef15b0fa549a216621aeff08a6ba46a
Parv3213 <parv3213@gmail.com>
2023-01-17 16:06:12 +0530

    Add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
-import '../styles/globals.css'
+import { Analytics } from '@vercel/analytics/react'
 import { ThemeProvider } from 'next-themes'
 import type { AppProps } from 'next/app'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <ThemeProvider attribute="class">
         <Component {...pageProps} />
+        <Analytics />
       </ThemeProvider>
     </>
   )
