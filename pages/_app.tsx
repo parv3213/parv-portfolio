5d598812e63c3f4039f4a4af896c019d1508fb57
Parv3213 <parv3213@gmail.com>
2025-03-04 12:13:28 +0700

    Replace Vercel Analytics with Speed Insights in the application

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import { ThemeProvider } from 'next-themes'
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
@@ -8,8 +8,8 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <ThemeProvider attribute="class">
         <Component {...pageProps} />
-        <Analytics />
       </ThemeProvider>
+      <SpeedInsights />
     </>
   )
 }
